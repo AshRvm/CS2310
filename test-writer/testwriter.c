ef2c43bb7d46d32527166d82a1cf66217ef14873
Vedaant Alok Arya <vedaant.arya.v3d3@gmail.com>
2020-08-31 17:08:00 +0530

    Added untested Parity chips. Updated testwriter to support buses.

--- test-writer/testwriter.c
+++ test-writer/testwriter.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 #define MAX_LEN 20
 #define MAX_FILE_LEN 20
@@ -9,13 +10,42 @@ int getBit(long long src, int position)  {
 	return (src >> position) & 1;
 }
 
+int parseInt(char * name, int start, int end)  {
+	int i;
+	int num = 0, numlength = 0;
+
+	for(i = start; i <= end; i++)  {
+		int digit = name[i] - '0';
+		num += pow(10, numlength++) * digit;
+	}
+
+	return num;
+}
+
+int busLength(char * name)  {
+	int length = strlen(name);
+	if(name[length - 1] != ']')  {
+		return 0;
+	}
+
+	int i = length - 2;
+	while(name[i] != '[')  {
+		i--;
+	}
+
+	return parseInt(name, i + 1, length - 2);
+}
+
 int main()  {
+	int mode;
+
 	int varcount = 0;
+	int busoverhead = 0;
 
 	printf("Enter number of variables: ");
 	scanf("%d", &varcount);
 
-	printf("\nEnter the variable names, one per line (max length %d):\n", MAX_LEN);
+	printf("\nEnter the variable names, one per line, for buses, enter busname[buslength] (max length %d):\n", MAX_LEN);
 
 	char * * varArr = (char * *) malloc(varcount * sizeof(char *));
 
@@ -23,6 +53,11 @@ int main()  {
 	for(i = 0; i < varcount; i++)  {
 		varArr[i] = (char *) malloc(MAX_LEN * sizeof(char));
 		scanf("%s", varArr[i]);
+
+		int bl = busLength(varArr[i]);
+		if(bl != 0)  {
+			busoverhead += bl - 1;
+		}
 	}
 
 	char fileName[MAX_FILE_LEN] = "";
@@ -32,21 +67,31 @@ int main()  {
 
 	FILE * outputFile = fopen(fileName, "w");
 
-	long long lim = pow(2, varcount);
+	long long lim = pow(2, varcount + busoverhead);
 	long long iter;
 	long long innerIter;
 
-	printf("\nWriting...\n");
+	printf("\nWriting... ");
 
 	for(iter = 0; iter < lim; iter++)  {
 		printf("%lld/%lld\n", iter, lim);
 
 		for(innerIter = 0; innerIter < varcount; innerIter++)  {
-			fprintf(outputFile, "set %s %d, ", varArr[innerIter], getBit(iter, varcount - innerIter - 1));
+			int bl = busLength(varArr[innerIter]);
+			if(bl == 0)  {
+				fprintf(outputFile, "set %s %d, ", varArr[innerIter], getBit(iter, varcount - innerIter - 1));
+			}  else  {
+				int busIter;
+				fprintf(outputFile, "set %s %%B", varArr[innerIter]);
+				for(busIter = 0; busIter < bl; busIter++)  {
+					fprintf(outputFile, "%d", getBit(iter, varcount - innerIter + busIter - 1));
+				}
+				fprintf(outputFile, ", ");
+			}
 		}
 
 		fprintf(outputFile, "eval, output;\n");
 	}
 
-	printf("\nDone.\n");
+	printf("Done.\n");
 }
